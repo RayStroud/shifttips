5b7380860c5f2f834676538c12665bc58e943d2a
RayStroud <ray.stroud@gmail.com>
2015-04-24 16:12:24 -0600

    added debug and tried to fix input styles

--- all.php
+++ all.php
@@ -100,6 +100,19 @@
 		</div>
 	</div>
 	<div id="footer">
+		<a href="#" target="popup" onClick="wopen('#', 'popup', 320, 480); return false;">DEBUG MOBILE - POPUP</a>
 	</div>
+	<script>
+		function wopen(url, name, w, h)
+		{
+			//Fudge factors for window decoration space.
+			w += 32;
+			h += 96;
+			var win = window.open(url, name, 'width=' + w + ', height=' + h + ', ' +
+				'location=no, menubar=no, ' + 'status=no, toolbar=no, scrollbars=no, resizable=no');
+			win.resizeTo(w, h);
+			win.focus();
+		}
+	</script>
 </body>
 </html>
