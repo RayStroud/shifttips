d97123eeb597b19229dec36f0be90ff3d7350ff0
Ray Stroud <ray.stroud@gmail.com>
2016-06-02 16:08:31 -0700

    CORS cross-domain database

--- data/include/db.php
+++ data/include/db.php
@@ -18,12 +18,14 @@
 		//* DEBUG */ else {echo 'php version: ' . phpversion() . '<br/>mysql version: ' . $db->get_server_info() . '<hr>';}
 
 		//allow access from domain
-		header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-		header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization");
-		header('Access-Control-Allow-Origin: http://raystroud.ca');
-		header('Access-Control-Allow-Origin: https://raystroud.ca');
-		header('Access-Control-Allow-Origin: http://www.raystroud.ca');
-		header('Access-Control-Allow-Origin: https://www.raystroud.ca');
+		$safeDomains = array('http://raystroud.ca', 'https://raystroud.ca', 'http://www.raystroud.ca', 'https://www.raystroud.ca', 'http://raystroud.com', 'https://raystroud.com', 'http://www.raystroud.com', 'https://www.raystroud.com');
+		$domain = $_SERVER['HTTP_ORIGIN'];
+		if (in_array($domain, $safeDomains, true))
+		{
+			header('Access-Control-Allow-Origin: ' . $domain);
+			header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+			header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization');
+		}
 		//* DEBUG */ header('Access-Control-Allow-Origin: *');
 	} 
 	catch (Exception $e) 
