1c43706e5a1ceac7cbbd0dbb37e813c359de2877
Ray Stroud <ray.stroud@gmail.com>
2016-08-12 14:11:37 -0700

    parsed text, TODO display parsed text

--- app/dayforce/dayforce.js
+++ app/dayforce/dayforce.js
@@ -5,8 +5,63 @@ angular.module('shiftTips')
 	// ctrl.prefs = filterService.getUserPrefs(ctrl.uid).dayforce;	//for future custom view
 
 	ctrl.parseFile = function(textData) {
-		alert("parse called!");
+		ctrl.parsedData = ctrl.parseVCalendar(textData);
 	}
+
+	ctrl.parseVCalendar = function(text) {
+		var events = [];	//to store all events
+		var event = {};		//to store current event
+
+		//loop through each line
+		text.split(/\r?\n/).forEach(function(line) {
+			var match;	//to store regex match
+
+			//if line is BEGIN:VEVENT
+			if(match = line.match(/^BEGIN:VEVENT/)) {
+				//make new event
+				event = {};
+			}
+
+			//if line is DTSTART
+			else if(match = line.match(/^DTSTART:(.*)/)) {
+				//parse date, startTime, yearweek, dayOfWeek, lunchDinner
+				var dtstart = match[1];
+				var momentDate = moment(dtstart, 'YYYYMMDDTHHmmssZ');
+				event.date = momentDate.format("YYYY-MM-DD");
+				event.startTime = momentDate.format("HH:mm:ss");
+				event.yearweek = momentDate.isoWeek();
+				event.dayOfWeek = momentDate.format("ddd");
+				event.lunchDinner = momentDate.hour < 2 ? "L" : "D";
+			}
+
+			//if line is DTEND
+			else if(match = line.match(/^DTEND:(.*)/)) {
+				//parse endTime
+				var dtend = match[1];
+				var momentDate = moment(dtend, 'YYYYMMDDTHHmmssZ');
+				event.endTime = momentDate.format("HH:mm:ss");
+			}
+
+			//if line is SUMMARY
+			else if(match = line.match(/^SUMMARY:(.*)/)) {
+				//store summary
+				event.summary = match[1];
+			}
+
+			//if line is LOCATION
+			else if(match = line.match(/^LOCATION:(.*)/)) {
+				//store location
+				event.location = match[1];
+			}
+
+			//if line is END:VEVENT
+			if(match = line.match(/^END:VEVENT/)) {
+				//make new event
+				events.push(event);
+			}
+		});
+		return events;
+	};
 }])
 
 .directive("textFile", [function() {
